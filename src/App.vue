c5f2dfe60a33ac183e81c8479f309adf0474bdac
StoneRiga <stoneriga@gmail.com>
2021-11-29 15:12:20 +0300

    version 1.0.0

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,26 @@
   </header>
   <main>
     <section class="player">
-      <h2 class="song-title">{{ current.title }} <span>{{ current.artist }}</span></h2>
+      <h2 class="song-title">{{ current.title }} - <span>{{ current.artist }}</span></h2>
+      <div class="controls">
+        <button class="prev" @click="prev">Prev</button>
+        <button class="play" v-if="!isPlaying" @click="play">Play</button>
+        <button class="pause" v-else @click="pause">Pause</button>
+        <button class="next" @click="next">Next</button> 
+      </div>
+    </section>
+    <section class="playlist">
+      <h3>Playlist</h3>
+      <button v-for="song in songs" :key="song.src" @click="play(song)"
+      :class="(song.src == current.src) ? 'song playing' : 'song'">
+      {{ song.title }} - {{ song.artist }}
+      </button>
     </section>
   </main>
 </div>
 
 </template>
-
+     
 <script>
 
 
@@ -26,9 +39,10 @@ export default {
         
       },
       index: 0,
+      isPlaying: false,
       songs: [
         {
-          title: 'City High [Ras Stone Remake]',
+          title: 'City High',
           artist: 'Inspectah Deck x Ras Stone',
           src: require('./assets/city.mp3')
         },
@@ -38,7 +52,7 @@ export default {
           src: require('./assets/era.mp3')
         },
         {
-          title: 'LAnd of The Free',
+          title: 'Land of The Free',
           artist: 'Ras Stone x Joey B@dass',
           src: require('./assets/land.mp3')
         }
@@ -46,9 +60,40 @@ export default {
       player: new Audio()
     }
   },
+  methods: {
+    play(song){
+      if (typeof song.src != "undefined"){
+        this.current =song;
+        this.player.src = this.current.src;
+      }
+      this.player.play();
+      this.isPlaying = true;
+    },
+    pause(){
+      this.player.pause();
+      this.isPlaying = false;
+    },
+    next(){
+      this.index++;
+      if(this.index > this.songs.length -1){
+        this.index = 0;
+      }
+      this.current = this.songs[this.index];
+        this.play(this.current);
+    },
+    prev(){
+      this.index--;
+      if(this.index < 0){
+        this.index = this.songs.length -1;
+      }
+      this.current = this.songs[this.index];
+      this.play(this.current);
+    }
+  },
   created(){
     this.current = this.songs[this.index];
     this.player.src = this.current.src;
+    this.player.play();
   }
 }
 </script>
