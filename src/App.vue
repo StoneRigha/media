8c4b0f874fb9d6fe38facc22f282119fdc15a879
StoneRiga <stoneriga@gmail.com>
2021-11-29 11:54:39 +0300

    version 1.0.0

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+  <header>
+    <h1>My Music</h1>
+  </header>
+  <main>
+    <section class="player">
+      <h2 class="song-title">{{ current.title }} <span>{{ current.artist }}</span></h2>
+    </section>
+  </main>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data(){
+    return{
+      current: {
+        
+      },
+      index: 0,
+      songs: [
+        {
+          title: 'City High [Ras Stone Remake]',
+          artist: 'Inspectah Deck x Ras Stone',
+          src: require('./assets/city.mp3')
+        },
+        {
+          title: 'Interstella Era',
+          artist: 'Ras Stone',
+          src: require('./assets/era.mp3')
+        },
+        {
+          title: 'LAnd of The Free',
+          artist: 'Ras Stone x Joey B@dass',
+          src: require('./assets/land.mp3')
+        }
+      ],
+      player: new Audio()
+    }
+  },
+  created(){
+    this.current = this.songs[this.index];
+    this.player.src = this.player.src;
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+} */
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: sans-serif;
+}
+header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 15px;
+  background-color: #212121;
+  color: #fff;
 }
 </style>
